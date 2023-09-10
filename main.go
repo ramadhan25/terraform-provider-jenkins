f413a80e55039ec6641e002c41cce4f75d21e54f
atwatanmalikm <atwatanmalikm@gmail.com>
2023-09-10 22:49:24 +0700

    fmt

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"github.com/atwatanmalikm/terraform-provider-jenkins/jenkins"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-	"github.com/atwatanmalikm/terraform-provider-jenkins/jenkins"
 )
 
 func main() {
